9b2ea305137dbe10848e07ac6e296458857d80d1
Shantel <snchristian8@gmail.com>
2021-10-29 21:14:58 -0400

    commit tree d609e76edfda433ef1b87a5831dbdf7bb31cfe33

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Links from "./Links"
+import user from "../data/user";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio || props.bio === "" ? <p>{user.bio}</p>:null}
+      {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links
+       github={user.links.github} linkedin={user.links.linkedin}
+      />
       {/* add your <Links /> component here */}
     </div>
   );
